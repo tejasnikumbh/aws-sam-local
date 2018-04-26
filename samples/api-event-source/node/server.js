a61faf46760d3b90eaaec523c35c8e0140633d34
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-04-26 16:11:44 +0530

    Added Notes for GET/POST API Route using AWS Lambda

--- samples/api-event-source/node/server.js
+++ samples/api-event-source/node/server.js
@@ -1,28 +1,33 @@
 'use strict';
 
+// NOTE:- Important Points
+// 1. event.body is received as string and should be converted into JSON by
+// using JSON.parse(event.body)
+// 2. While sending response, we should also send the body as a JSON string using
+// JSON.stringify on our JSON object that we want to send as the body
+
+// Simply returns a sample JSON object after stringifying it.
 exports.getHandler = (event, context, callback) => {
-  // console.log(`Context: ${context}`);
-  var bodyObject = {
+  var bodyJSONObject = {
     user: {
       id: "1",
       name: "Tejas",
       age: 26
     }
   };
-  var bodyObjectString = JSON.stringify(bodyObject);
-  // console.log(bodyObject);
-  // console.log(bodyObjectString);
+  var bodyJSONString = JSON.stringify(bodyJSONObject);
   callback(null, {
     statusCode:200,
-    body: bodyObjectString
+    body: bodyJSONString
   });
 }
 
-
+// Returns the JSON string sent as the body in the post call
 exports.postHandler = (event, context, callback) => {
-  console.log(JSON.parse(event.body));
+  var bodyJSONString = event.body;
+  var bodyJSONObject = JSON.parse(event.body);
   callback(null, {
-    statusCode:200,
-    body: event.body
+    statusCode: 200,
+    body: bodyJSONString
   });
 }
